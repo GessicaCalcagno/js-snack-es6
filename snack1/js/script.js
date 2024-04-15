3b2b203e6d8874038d87c2e2783df2079d4318f2
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-04-15 15:48:54 +0200

    Add: scaffolding snack1

---
+++ snack1/js/script.js
@@ -0,0 +1,7 @@
+// TEST
+alert('ciao')
+
+//************************************************ */
+// Lo usiamo come controllo alla fine se c'è qualche problema di sintassi!
+//"use strict";
+//**************************************************** */
