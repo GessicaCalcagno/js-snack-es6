535f24da0a3e6d734e4d0c56bd231c2a6c84a841
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-04-16 12:15:43 +0200

    Add: snack 4

--- snack 4/js/script.js
+++ snack 4/js/script.js
@@ -6,7 +6,6 @@
 //"use strict";
 //**************************************************** */
 
-
 const automobili = [
   { marca: "Toyota", modello: "Yaris", alimentazione: "ibrida" },
   { marca: "Volkswagen", modello: "Golf", alimentazione: "benzina" },
@@ -17,6 +16,42 @@ const automobili = [
   { marca: "Ford", modello: "Focus", alimentazione: "benzina" },
   { marca: "Audi", modello: "A3", alimentazione: "diesel" },
   { marca: "Mercedes-Benz", modello: "EQC", alimentazione: "elettrico" },
-  { marca: "Opel", modello: "Corsa", alimentazione: "gpl" }
+  { marca: "Opel", modello: "Corsa", alimentazione: "gpl" },
 ];
 
+let benzina = [];
+let disel = [];
+let altro = [];
+
+benzina = automobili.filter((curAuto) => {
+  if (curAuto.alimentazione === "benzina") {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+console.log(benzina);
+
+disel = automobili.filter((curAuto) => {
+  if (curAuto.alimentazione === "diesel") {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+console.log(disel);
+
+altro = automobili.filter((curAuto) => {
+  if (
+    curAuto.alimentazione !== "benzina" &&
+    curAuto.alimentazione !== "diesel"
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+console.log(altro);
